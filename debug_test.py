12ce4d6aa32f9d4718954b72590198d6e8222596
preangeleo <preangelleo@gmail.com>
2024-12-05 16:34:04 +0800

    nothing special

--- debug_test.py
+++ debug_test.py
@@ -1,3 +1,2 @@
 from ghost_blog import *
 
-# auto_blog_post(DOLLARPLUS_CHAT_ID, engine, os.getenv("TELEGRAM_BOT_TOKEN_ENSPIRING"), ASSISTANT_MAIN_MODEL_BEST, user_parameters_realtime(DOLLARPLUS_CHAT_ID, engine))
